348bb92f177959a346a5be9936831501a4fb268f
ZihanYuan <u7773880@anu.edu.au>
2024-04-19 00:06:19 +1000

    finish add page, and connect it to add button in MainActivity4

--- src/app/src/main/java/com/example/eatsy/MainActivity4.java
+++ src/app/src/main/java/com/example/eatsy/MainActivity4.java
@@ -1,6 +1,9 @@
 package com.example.eatsy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity4 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextView addPageTextView = findViewById(R.id.add_pg);
+        addPageTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity4.this,Addpage.class);
+                startActivity(intent);
+            }
         });
     }
 }
