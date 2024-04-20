ade8fa62443cb418ed4191e49dfc346be9610a92
Vishakha <u7663368@anu.edu.au>
2024-04-20 17:00:24 +1000

    constraints

--- src/app/src/main/java/com/example/eatsy/DashboardActivity.java
+++ src/app/src/main/java/com/example/eatsy/DashboardActivity.java
@@ -11,13 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity4 extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.activity_dashboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,7 +27,7 @@ public class MainActivity4 extends AppCompatActivity {
         addPageTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity4.this,Addpage.class);
+                Intent intent = new Intent(DashboardActivity.this,Addpage.class);
                 startActivity(intent);
             }
         });
--- src/app/src/main/java/com/example/eatsy/LoginActivity.java
+++ src/app/src/main/java/com/example/eatsy/LoginActivity.java
@@ -7,21 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.example.eatsy.LoginUser;
 
 
-public class MainActivity2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
     Button loginBtn;
@@ -30,29 +27,29 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
         loginBtn = findViewById(R.id.login);
         username = findViewById(R.id.Email);
         password = findViewById(R.id.user_pass);
         firebaseAuth = FirebaseAuth.getInstance();
         if (firebaseAuth.getCurrentUser() != null)
-            startActivity(new Intent(getApplicationContext(), MainActivity4.class));
+            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = username.getText().toString().trim();
                 String pswrd = password.getText().toString().trim();
                 if (email.isEmpty() && pswrd.isEmpty()) {
-                    Toast.makeText(MainActivity2.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (email.isEmpty()) {
-                    Toast.makeText(MainActivity2.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if (pswrd.isEmpty()) {
-                    Toast.makeText(MainActivity2.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
 
                     return;
                 }
@@ -64,13 +61,13 @@ public class MainActivity2 extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
 
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(MainActivity2.this,
+                                    Toast.makeText(LoginActivity.this,
                                     "Login Successful", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(getApplicationContext(), MainActivity4.class));
+                                    startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
                                 } else {
-                                    Toast.makeText(MainActivity2.this,
+                                    Toast.makeText(LoginActivity.this,
                                     "Authentication failed", Toast.LENGTH_SHORT).show();
-                                    LoginUser.getInstance().error(MainActivity2.class, task.getException());
+                                    LoginUser.getInstance().error(LoginActivity.class, task.getException());
                                 }
                             }
                         });
