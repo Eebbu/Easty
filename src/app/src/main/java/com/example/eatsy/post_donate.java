e12d5d3c73b174453436fde9726389d95d49e65f
ZihanYuan <u7773880@anu.edu.au>
2024-04-26 22:32:46 +1000

    fix image problems, and add part of upload images

--- src/app/src/main/java/com/example/eatsy/post_donate.java
+++ src/app/src/main/java/com/example/eatsy/post_donate.java
@@ -2,14 +2,28 @@ package com.example.eatsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class post_donate extends AppCompatActivity {
-
+    ArrayList paths = null;
+    ArrayList names= null;
+    List<Map<String, Object>> listItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +39,36 @@ public class post_donate extends AppCompatActivity {
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
+        ImageButton uploadImage = findViewById(R.id.cameraButton);
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                getImagePath();
+            }
+        });
+
+    }
+    void getImagePath(){
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            String path = cursor.getString(column_index);
+            paths.add(path);
+            names.add(name);
+        }
+        for (int i = 0; i < paths.size(); i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", paths.get(i));
+            map.put("desc", names.get(i));
+            listItems.add(map);
+        }
+    }
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 }
