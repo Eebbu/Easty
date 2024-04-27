4b57f0934857da3c17e0347b9a46570159ec3429
Jinyang Zeng <u7727175@anu.edu.au>
2024-04-27 11:55:17 +1000

    new postCard activity

---
+++ src/app/src/main/java/com/example/eatsy/postCard.java
@@ -0,0 +1,14 @@
+package com.example.eatsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_card);
+    }
+}
