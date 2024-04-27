b20f34ccb83c5115ce02f511a7c3d037963168b5
Jinyang Zeng <u7727175@anu.edu.au>
2024-04-27 13:55:56 +1000

    Finished postCard activity and connect Dashborad and postCard

--- src/app/src/main/java/com/example/eatsy/postCard.java
+++ src/app/src/main/java/com/example/eatsy/postCard.java
@@ -3,6 +3,8 @@ package com.example.eatsy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class postCard extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class postCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_card);
+        ImageButton go_back = findViewById(R.id.leftArrowButton);
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
