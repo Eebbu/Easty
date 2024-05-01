4d766a8b1539a99b30ee3ead94eb61f1ecd26605
Boxuan Lin <u7705128@anu.edu.au>
2024-05-01 14:46:57 +1000

    create PostDataDownloader.java , change the HashMap into ConcurrentHashmap. Now we can get the data from the firestore.

--- src/app/src/main/java/com/example/eatsy/MyApplication.java
+++ src/app/src/main/java/com/example/eatsy/MyApplication.java
@@ -18,34 +18,64 @@ public class MyApplication extends Application {
         super.onCreate();
         FirebaseApp.initializeApp(this);
 
-        FirebaseFirestore db = FirestoreHelper.getFirestore();
         CollectionReference usersCollectionRef = FirestoreHelper.getCollectionRef("users");
         UserDataDownloader downloader = new UserDataDownloader();
-        downloader.downloadData(usersCollectionRef, new DataDownloadCallback<userFT>() {
-            @Override
-            public void onDataReceived(HashMap<String, userFT> data) {
 
-                HashMap<String, userFT> userHashMap = downloader.getUserHashMap();
-                for (Map.Entry<String, userFT> entry : data.entrySet()) {
-                    String email = entry.getKey();
-                    userFT user = entry.getValue();
+        // Get the posts collection reference
+        CollectionReference postsCollectionRef = FirestoreHelper.getCollectionRef("posts");
+        PostDataDownloader postDownloader = new PostDataDownloader();
 
-                    // printing the data(only for testing purpose)
-                    System.out.println("Email: " + email);
-                    System.out.println("Username: " + user.getUsername());
-                    System.out.println("Post IDs: " + user.getPostID());
-                    System.out.println("Photo_URL: " + user.getPhotoURL());
-                    System.out.println("------------------------");
-                }
-            }
+        // Call the asynchronous method, and handle the returned CompletableFuture
+        downloader.downloadData(usersCollectionRef)
+                .thenAccept(data -> {
+                    // Success: data received
+                    for (Map.Entry<String, userFT> entry : data.entrySet()) {
+                        String email = entry.getKey();
+                        userFT user = entry.getValue();
 
-            @Override
-            public void onDataDownloadFailed(Exception exception) {
-                // dealing with downloading fail case.
-                Log.e("DataDownloader", "download fail...", exception);
-            }
-        });
+                        // Printing the data, only for testing purposes
+                        System.out.println("Email: " + email);
+                        System.out.println("Username: " + user.getUsername());
+                        System.out.println("Post IDs: " + user.getPostID()); // Ensure correct method name
+                        System.out.println("Photo URL: " + user.getPhotoURL());
+                        System.out.println("------------------------");
+                    }
+                })
+                .exceptionally(ex -> {
+                    // Handle the case of download failure
+                    System.err.println("Data download failed: " + ex.getMessage());
+                    return null; // Return a value of the same type as thenAccept
+                });
+        // Download and handle post data
+        postDownloader.downloadData(postsCollectionRef)
+                .thenAccept(data -> {
+                    // Success: post data received
+                    for (Map.Entry<String, PostFT> entry : data.entrySet()) {
+                        String postID = entry.getKey();
+                        PostFT post = entry.getValue();
+
+                        // Printing the post data, only for testing purposes
+                        System.out.println("Post ID: " + postID);
+                        System.out.println("User Name: " + post.getUserName());
+                        System.out.println("Post Type: " + post.getPostType());
+                        System.out.println("Post Title: " + post.getPostTitle());
+                        System.out.println("Description: " + post.getPostDescription());
+                        System.out.println("Quantity: " + post.getQuantity());
+                        System.out.println("Pick Up Times: " + post.getPickUpTimes());
+                        System.out.println("Latitude: " + post.getLatitude());
+                        System.out.println("Longitude: " + post.getLongitude());
+                        System.out.println("Images: " + post.getImages());
+                        System.out.println("------------------------");
+                    }
+                })
+                .exceptionally(ex -> {
+                    // Handle the case of post data download failure
+                    System.err.println("Post data download failed: " + ex.getMessage());
+                    return null;
+                });
+    }
+
 
 
     }
-}
+
