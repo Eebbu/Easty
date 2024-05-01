a0c574210264f36a5d22f4b98d663650da8e52d1
Vishakha <u7663368@anu.edu.au>
2024-05-01 19:56:12 +1000

    fetch data from firebase

--- src/app/src/main/java/com/example/eatsy/ProfileActivity.java
+++ src/app/src/main/java/com/example/eatsy/ProfileActivity.java
@@ -1,16 +1,20 @@
 package com.example.eatsy;
 
 import static com.example.eatsy.R.id.Signout;
+import static com.example.eatsy.R.id.back_btn;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,15 +22,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView FullName, userEmail;
+    TextView userName, userEmail;
+
+    FirebaseAuth firebaseAuth;
 
     FirebaseFirestore firebaseFirestore;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    String userID;
     Button logOutBtn;
+    ImageView backButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -44,8 +59,43 @@ public class ProfileActivity extends AppCompatActivity {
 
 
         logOutBtn = findViewById(R.id.Signout);
-        FullName = findViewById(R.id.name_person);
+        userName = findViewById(R.id.name_person);
         userEmail = findViewById(R.id.email_on_profile);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        userID = firebaseAuth.getCurrentUser().getUid();
+        backButton = findViewById(back_btn);
+
+        if (user != null) {
+            firebaseFirestore.collection("users").document(user.getUid()).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot ds = task.getResult();
+                    if (ds.exists()) {
+                        String nameOfPerson = ds.getString("name");
+                        String emailOfPerson = ds.getString("email");
+                        userName.setText(nameOfPerson);
+                        userEmail.setText(emailOfPerson);
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                }
+
+                    else {
+                        Log.d("TAG", "get failed with", task.getException());
+                    }
+
+            });
+        }
+
+//        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//                userName.setText(value.getString("name"));
+//                userEmail.setText(value.getString("email"));
+//            }
+//        });
 
 
 
@@ -61,6 +111,16 @@ public class ProfileActivity extends AppCompatActivity {
                         "You have been signed out", Toast.LENGTH_SHORT).show();
             }
         });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
