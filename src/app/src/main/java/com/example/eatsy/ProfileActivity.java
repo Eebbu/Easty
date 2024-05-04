e820a7f8928a2f99da68d49fd0c904149055922f
Vishakha <u7663368@anu.edu.au>
2024-05-04 11:22:46 +1000

    signOut function

--- src/app/src/main/java/com/example/eatsy/ProfileActivity.java
+++ src/app/src/main/java/com/example/eatsy/ProfileActivity.java
@@ -134,6 +134,8 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
                 startActivity(intent);
                 Toast.makeText(ProfileActivity.this,
                         "You have been signed out", Toast.LENGTH_SHORT).show();
