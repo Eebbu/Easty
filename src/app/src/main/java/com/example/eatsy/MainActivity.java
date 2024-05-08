f4b3d52867ec46f77e05806012c8d49b6eebe672
XILIN120408 <xil20222@163.com>
2024-05-08 16:34:52 +1000

    Revised button name for filtering

--- src/app/src/main/java/com/example/eatsy/MainActivity.java
+++ src/app/src/main/java/com/example/eatsy/MainActivity.java
@@ -14,7 +14,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 /** Functionality
  * This is the main page which appears when user opens the app.
  * It has title of the app and a login button that takes us to the login page.
- * @author Vishakha Mathur(u7663368)
+ * Loading data from firebase
+ * @author Vishakha Mathur(u7663368) and Lin Xi(u7777752)
  */
 public class MainActivity extends AppCompatActivity {
 
