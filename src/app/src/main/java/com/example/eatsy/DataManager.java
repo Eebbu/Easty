8e41fffbb57a7afbbae2632992916bf4a898c6f4
XILIN120408 <xil20222@163.com>
2024-05-10 13:20:19 +1000

    Added some comments

--- src/app/src/main/java/com/example/eatsy/DataManager.java
+++ src/app/src/main/java/com/example/eatsy/DataManager.java
@@ -3,12 +3,21 @@ package com.example.eatsy;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * This class implements a singleton pattern for
+ * managing and storing the mapping relationship
+ * between user (userFT) and post (Post) data
+ * Author: Boxuan Lin(u7705128), Lin Xi(u7777752) and Jinyang Zeng(u7727175)
+ */
 public class DataManager {
     private static final DataManager instance = new DataManager();
     private ConcurrentHashMap<String, userFT> userHashMap = new ConcurrentHashMap<>();
     private ConcurrentHashMap<String, Post> postHashMap = new ConcurrentHashMap<>();
 
-    private DataManager() {}
+    //Make sure it has only one instance through a private constructor
+    private DataManager() {
+
+    }
 
     public static DataManager getDataInstance() {
         return instance;
