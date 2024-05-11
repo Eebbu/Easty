d76e433ce2a1bd79284218c2076109a3990c2432
Jinyang Zeng <u7727175@anu.edu.au>
2024-05-12 03:14:06 +1000

    update from main

--- src/app/src/main/java/com/example/eatsy/Addpage.java
+++ src/app/src/main/java/com/example/eatsy/Addpage.java
@@ -15,7 +15,7 @@ import android.view.View;
 /** Add page UI
  * @author Zihan Yuan(u7773880)
  */
-public class Addpage extends AppCompatActivity {
+public class AddPage extends AppCompatActivity {
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -28,49 +28,37 @@ public class Addpage extends AppCompatActivity {
         NestedScrollView nestedScrollView = findViewById(R.id.Scroll);
 
         // Set an onTouchListener on the ConstraintLayout
-        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Check if the touch event is outside the NestedScrollView
-                    Rect rect = new Rect();
-                    nestedScrollView.getGlobalVisibleRect(rect);
-                    if (!rect.contains((int) event.getRawX(), (int) event.getRawY())) {
-                        finish(); // Go back to the previous Activity
-                        return true; // Event is handled
-                    }
+        constraintLayout.setOnTouchListener((view, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                // Check if the touch event is outside the NestedScrollView
+                Rect rect = new Rect();
+                nestedScrollView.getGlobalVisibleRect(rect);
+                if (!rect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    finish(); // Go back to the previous Activity
+                    return true; // Event is handled
                 }
-                return false; // Event is not handled, pass it to the NestedScrollView or other child views
             }
+            return false; // Event is not handled, pass it to the NestedScrollView or other child views
         });
 
         CardView donate_card = findViewById(R.id.donateCard);
-        donate_card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start post_donate activity
-                Intent intent = new Intent(Addpage.this, Post_donate.class);
-                startActivity(intent);
-            }
+        donate_card.setOnClickListener(v -> {
+            // Start post_donate activity
+            Intent intent = new Intent(AddPage.this, post_donate.class);
+            startActivity(intent);
         });
         // Setup click listeners for card views to navigate to specific post creation activities
         CardView exchange_card = findViewById(R.id.exchangeCard);
-        exchange_card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start post_exchange activity
-                Intent intent = new Intent(Addpage.this, Post_exchange.class);
-                startActivity(intent);
-            }
+        exchange_card.setOnClickListener(v -> {
+            // Start post_exchange activity
+            Intent intent = new Intent(AddPage.this, post_exchange.class);
+            startActivity(intent);
         });
         CardView wanted_card = findViewById(R.id.wantedCard);
-        wanted_card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start post_wanted activity
-                Intent intent = new Intent(Addpage.this, Post_wanted.class);
-                startActivity(intent);
-            }
+        wanted_card.setOnClickListener(v -> {
+            // Start post_wanted activity
+            Intent intent = new Intent(AddPage.this, post_wanted.class);
+            startActivity(intent);
         });
     }
 
